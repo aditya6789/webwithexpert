d64b63a69974182680c104ddf850ba418302b796
aditya6789 <paswanaditya256@gmail.com>
2024-12-27 15:26:48 +0530

    error fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from "path"
 
 // https://vitejs.dev/config/
@@ -10,4 +10,26 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    sourcemap: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'framer-motion': ['framer-motion'],
+          'three-vendor': ['three', 'three-stdlib'],
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['three-stdlib']
+  },
+  esbuild: {
+    exclude: ['three-stdlib'],
+    logOverride: {
+      'this-is-undefined-in-esm': 'silent'
+    }
+  }
 })
